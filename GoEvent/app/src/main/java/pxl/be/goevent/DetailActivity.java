498dd9a4f3ba8dab41d5423ff0d6e1654b6eb74d
kimprzybylski <Kim.Przybylski@student.pxl.be>
2017-10-13 23:24:26 +0200

    Layout menu toegevoegd

--- GoEvent/app/src/main/java/pxl/be/goevent/DetailActivity.java
+++ GoEvent/app/src/main/java/pxl/be/goevent/DetailActivity.java
@@ -2,6 +2,9 @@ package pxl.be.goevent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,4 +13,17 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home, menu);
+        return true;
+    }
+
+    //TODO::menu items nog laten werken
+    /*public boolean onOptionsItemSelected(MenuItem item) {
+
+        //respond to menu item selection
+
+    }*/
 }
--- GoEvent/app/src/main/java/pxl/be/goevent/HomeActivity.java
+++ GoEvent/app/src/main/java/pxl/be/goevent/HomeActivity.java
@@ -1,7 +1,11 @@
 package pxl.be.goevent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.map:
+                startActivity(new Intent(this, EventsActivity.class));
+                return true;
+            case R.id.myevents:
+                startActivity(new Intent(this, DetailActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
