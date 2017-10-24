582c356ec31130710376d1c55897083ec23a1362
YannickVanhoof <yannick.vanhoof@hotmail.com>
2017-10-24 12:09:59 +0200

    Json parser returns events or users

--- GoEvent/app/src/main/java/pxl/be/goevent/HomeActivity.java
+++ GoEvent/app/src/main/java/pxl/be/goevent/HomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -15,14 +16,32 @@ import android.widget.ListView;
 
 import com.facebook.login.LoginManager;
 
+import org.json.JSONException;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+       ApiCaller caller = new ApiCaller();
+        try {
+            String json =caller.execute("http://goevent.azurewebsites.net/api/Event/1 ").get();
+            JsonParser parser = new JsonParser();
+            Event events = parser.JsonToEvent(json);
+            Log.d("Evenement" , events.toString());
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         if (savedInstanceState == null) {
--- GoEvent/app/src/main/java/pxl/be/goevent/JsonParser.java
+++ GoEvent/app/src/main/java/pxl/be/goevent/JsonParser.java
@@ -6,39 +6,109 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.sql.Time;
+import java.util.Date;
 
 /**
  * Created by 11500046 on 24/10/2017.
  */
 
 public class JsonParser {
-    public String[] PrintJson(String jsonString)
+    public Event[] JsonToEventArray(String jsonString)
             throws JSONException {
 
-        //JSONObject event = new JSONObject(jsonString);
-        //JSONArray events = event.getJSONArray("list");
         JSONArray events = new JSONArray(jsonString);
-        String[] resultStrs = new String[events.length()];
+        Event[] results = new Event[events.length()];
         for (int i = 0; i < events.length(); i++) {
             // Get the JSON object representing the day
             JSONObject e = events.getJSONObject(i);
-
             // description is in a child array called "weather", which is 1 element long.
             // JSONObject weatherObject = e.getJSONArray(OWM_WEATHER).getJSONObject(0);
             // description = weatherObject.getString(OWM_DESCRIPTION);
-
-           // JSONObject temperatureObject = e.getJSONObject(OWM_TEMPERATURE);
-            int id = e.getInt("Id");
-            String name = e.getString("Name");
-            resultStrs[i] = id + " - " + name;
+            Event result = new Event();
+            result.setAttendees(null);
+            result.setCity(e.getString("City"));
+            result.setCoverPhoto(null);
+            result.setDate(null);
+            result.setDescription(e.getString("Description"));
+            result.setEndTime(null);
+            result.setHouseNumber(e.getInt("houseNumber"));
+            result.setId(e.getInt("Id"));
+            result.setLatitude(e.getDouble("Latitude"));
+            result.setLongitude(e.getDouble("Longitude"));
+            result.setName(e.getString("Name"));
+            result.setOrganisator(null);
+            result.setStreet(e.getString("Street"));
+            result.setVenue(e.getString("Venue"));
+            result.setPostalCode(e.getInt("PostalCode"));
+            results[i] = result;
         }
+        return results;
 
-        for (String s : resultStrs) {
-            Log.v("JSONPARSER", "" + s);
+    }
+
+    public Event JsonToEvent(String jsonString)
+            throws JSONException {
+
+            JSONObject event = new JSONObject(jsonString);
+            Event result = new Event();
+            result.setAttendees(null);
+            result.setCity(event.getString("City"));
+            result.setCoverPhoto(null);
+            result.setDate(null);
+            result.setDescription(event.getString("Description"));
+            result.setEndTime(null);
+            result.setHouseNumber(event.getInt("houseNumber"));
+            result.setId(event.getInt("Id"));
+            result.setLatitude(event.getDouble("Latitude"));
+            result.setLongitude(event.getDouble("Longitude"));
+            result.setName(event.getString("Name"));
+            result.setOrganisator(null);
+            result.setStreet(event.getString("Street"));
+            result.setVenue(event.getString("Venue"));
+            result.setPostalCode(event.getInt("PostalCode"));
+
+        return result;
+
+    }
+
+    public AppUser JsonToAppUser(String jsonString) throws JSONException {
+        AppUser result = new AppUser();
+        JSONObject user = new JSONObject(jsonString);
+        result.setId(user.getInt("Id"));
+        result.setUserName(user.getString("UserName"));
+        result.setPassword(user.getString("Password"));
+        result.setEmail(user.getString("Email"));
+        result.setFirstname(user.getString("Firstname"));
+        result.setLastName(user.getString("Lastname"));
+        result.setAddress(user.getString("Adress"));
+        result.setCity(user.getString("City"));
+        result.setPostalCode(user.getInt("PostalCode"));
+        result.setOrganisedEvents(null);
+        result.setEvents(null);
+        return result;
+    }
+
+    public AppUser[] JsonToAppUserArray(String jsonString) throws JSONException {
+
+        JSONArray users = new JSONArray(jsonString);
+        AppUser[] results = new AppUser[users.length()];
+        for (int i =0; i < users.length() ; i++){
+            AppUser result = new AppUser();
+            JSONObject user = users.getJSONObject(i);
+            result.setId(user.getInt("Id"));
+            result.setUserName(user.getString("UserName"));
+            result.setPassword(user.getString("Password"));
+            result.setEmail(user.getString("Email"));
+            result.setFirstname(user.getString("Firstname"));
+            result.setLastName(user.getString("Lastname"));
+            result.setAddress(user.getString("Adress"));
+            result.setCity(user.getString("City"));
+            result.setPostalCode(user.getInt("PostalCode"));
+            result.setOrganisedEvents(null);
+            result.setEvents(null);
+            results[i] = result;
         }
-        return resultStrs;
-
+        return results;
     }
 }
 
