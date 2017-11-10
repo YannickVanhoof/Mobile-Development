5cfe44ddb040443dff2fd2383bca8de457162ed0
YannickVanhoof <yannick.vanhoof@hotmail.com>
2017-11-10 17:29:23 +0100

    Layout fixes

--- GoEvent/app/src/main/java/pxl/be/goevent/Activities/RegisterActivity.java
+++ GoEvent/app/src/main/java/pxl/be/goevent/Activities/RegisterActivity.java
@@ -19,31 +19,8 @@ public class RegisterActivity extends Application {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.home, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.map:
-                startActivity(new Intent(this, MapsActivity.class));
-                return true;
-            case R.id.myevents:
-                startActivity(new Intent(this, HomeActivity.class));
-                return true;
 
-            case R.id.logout:
-                LoginManager.getInstance().logOut();
-                setUser(null);
-                startActivity(new Intent(this, MainActivity.class));
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+
 }
 
